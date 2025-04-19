bc7e4b85b00c5c30e3a8ae66ee15d5af86add617
connerohnesorge <conneroisu@outlook.com>
2025-04-19 06:39:22 -0500

    added known header file for student_attitude_controller.c

---
+++ src/student_attitude_controller.h
@@ -0,0 +1,98 @@
+#ifndef STUDENT_ATTITUDE_CONTROLLER_H_
+#define STUDENT_ATTITUDE_CONTROLLER_H_
+
+#include "commander.h"
+#include <stdbool.h>
+
+/**
+ * @brief Initialize PID data structures with coefficients defined in `pid.h`.
+ *
+ * @param updateDt expected delta time since last call for all PID loops
+ */
+void studentAttitudeControllerInit(const float updateDt);
+
+/**
+ * @brief Simple test to make sure controller is initialized
+ *
+ * @return true/false
+ */
+bool studentAttitudeControllerTest(void);
+
+/**
+ * Make the controller run an update of the attitude PID. The output is
+ * the desired rate which should be fed into a rate controller. The
+ * attitude controller can be run in a slower update rate then the rate
+ * controller.
+ *
+ * @param rollAngleMeasured input, the measured roll angle
+ * @param pitchAngleMeasured input, the measured pitch angle
+ * @param yawAngleMeasured input, the measured yaw angle
+ * @param rollAngleDesired input, the desired roll angle
+ * @param pitchAngleDesired input, the desired pitch angle
+ * @param yawAngleDesired input, the desired yaw angle
+ * @param rollRateDesired output, the calculated desired roll rate
+ * @param pitchRateDesired output, the calculated desired pitch rate
+ * @param yawRateDesired output, the calculated desired yaw rate
+ */
+void studentAttitudeControllerCorrectAttitudePID( //
+    float rollAngleMeasured,                      //
+    float pitchAngleMeasured,                     //
+    float yawAngleMeasured,                       //
+    float rollAngleDesired,                       //
+    float pitchAngleDesired,                      //
+    float yawAngleDesired,                        //
+    float *rollRateDesired,                       //
+    float *pitchRateDesired,                      //
+    float *yawRateDesired                         //
+);
+
+/**
+ * Make the controller run an update of the rate PID. Input comes from the
+ * correct attitude function. The output is the actuator force.
+ *
+ * TODO MICROCART: Add output to this function to better line up with the other
+ * controllers
+ *
+ * @param rollRateMeasured input, the measured roll rate
+ * @param pitchRateMeasured input, the measured pitch rate
+ * @param yawRateMeasured input, the measured yaw rate
+ * @param rollRateDesired input, the desired roll rate
+ * @param pitchRateDesired input, the desired pitch rate
+ * @param yawRateDesired input, the desired yaw rate
+ * @param rollCommand output, the calculated roll command
+ * @param pitchCommand output, the calculated pitch command
+ * @param yawCommand output, the calculated yaw command
+ */
+void studentAttitudeControllerCorrectRatePID( //
+    float rollRateMeasured,                   //
+    float pitchRateMeasured,                  //
+    float yawRateMeasured,                    //
+    float rollRateDesired,                    //
+    float pitchRateDesired,                   //
+    float yawRateDesired,                     //
+    int16_t *rollCommand,                     //
+    int16_t *pitchCommand,                    //
+    int16_t *yawCommand                       //
+);
+
+/**
+ * Reset controller roll attitude PID
+ */
+void studentAttitudeControllerResetRollAttitudePID(void);
+
+/**
+ * Reset controller pitch attitude PID
+ */
+void studentAttitudeControllerResetPitchAttitudePID(void);
+
+/**
+ * Reset controller yaw attitude PID
+ */
+void studentAttitudeControllerResetYawAttitudePID(void);
+
+/**
+ * Reset controller roll, pitch and yaw PID's.
+ */
+void studentAttitudeControllerResetAllPID(void);
+
+#endif /* STUDENT_ATTITUDE_CONTROLLER_H_ */
