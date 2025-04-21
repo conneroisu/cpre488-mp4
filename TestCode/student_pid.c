d4d4cfc37c7fbdb86e2a8c3ae92392e8fe699c9b
Owen Parker <54323555+OParker5@users.noreply.github.com>
2025-04-20 22:23:37 -0500

    Update student_pid.c

--- TestCode/student_pid.c
+++ TestCode/student_pid.c
@@ -1,204 +1,292 @@
+#include <stdbool.h>
+#include "FreeRTOS.h"
+#include "student_attitude_controller.h"
 #include "student_pid.h"
-#include "num.h"
+#include "param.h"
+#include "log.h"
 #include <math.h>
-#include <float.h>
 
-/**
- * PID object initialization.
- *
- * @param[out] pid   A pointer to the pid object to initialize.
- * @param[in] desired  The initial set point.
- * @param[in] kp        The proportional gain
- * @param[in] ki        The integral gain
- * @param[in] kd        The derivative gain
- * @param[in] dt        Delta time since the last call
- * @param[in] samplingRate Frequency the update will be called
- * @param[in] cutoffFreq   Frequency to set the low pass filter cutoff at
- * @param[in] enableDFilter Enable setting for the D lowpass filter
- */
-void studentPidInit(PidObject* pid, const float desired, const float kp,
-             const float ki, const float kd, const float dt,
-             const float samplingRate, const float cutoffFreq,
-             bool enableDFilter)
+#define ATTITUDE_LPF_CUTOFF_FREQ 15.0f
+#define ATTITUDE_LPF_ENABLE false
+#define ATTITUDE_RATE_LPF_CUTOFF_FREQ 30.0f
+#define ATTITUDE_RATE_LPF_ENABLE false
+
+static PidObject pidRollRate;
+static PidObject pidPitchRate;
+static PidObject pidYawRate;
+
+static PidObject pidRoll;
+static PidObject pidPitch;
+static PidObject pidYaw;
+
+static bool isInit;
+
+static inline int16_t saturateSignedInt16(float in)
 {
-  //488 TODO initialize all the values in the PidObject struct
- pid.desired = 0;
- pid.kp = 0;
- pid.ki = 0;
- pid.ki = 0;
- pid.dt = 0;
- pid.samplingRate = 0;
- pid.cutoffFreq = 0;
-
-
-
-  //additional initialization for optional low pass filter
-  pid->enableDFilter = enableDFilter;
-  if (pid->enableDFilter)
-  {
-    lpf2pInit(&pid->dFilter, samplingRate, cutoffFreq);
-  }
+  if (in > INT16_MAX)
+    return INT16_MAX;
+  else if (in < -INT16_MAX)
+    return -INT16_MAX;
+  else
+    return (int16_t)in;
 }
 
-/**
- * Update the PID parameters.
- *
- * @param[in] pid         A pointer to the pid object.
- * @param[in] measured    The measured value
- * @param[in] updateError Set to TRUE if error should be calculated.
- *                        Set to False if studentPidSetError() has been used.
- * @return PID algorithm output
- */
-float studentPidUpdate(PidObject* pid, const float measured, const bool updateError)
+void studentAttitudeControllerInit(const float updateDt)
 {
+  if (isInit)
+    return;
 
-  // 488 TODO write base PID algorithm
-  if(updateError){
-      pid->error = pid->setpoint-measured;
-  }
-    // 488 TODO optionally enable derivative low pass filtering
-    /*
-    if (pid->enableDFilter)
-    {
-      pid->deriv = lpf2pApply(&pid->dFilter, deriv);
-      if (isnan(pid->deriv)) {
-        pid->deriv = 0;
-      }
-    } else {
-      pid->deriv = deriv;
-    }
-    */
+  studentPidInit(&pidRollRate, RATE_ROLL_KP, RATE_ROLL_KI, RATE_ROLL_KD, updateDt);
+  studentPidSetIntegralLimit(&pidRollRate, 0);
+  
+  studentPidInit(&pidPitchRate, RATE_PITCH_KP, RATE_PITCH_KI, RATE_PITCH_KD, updateDt);
+  studentPidSetIntegralLimit(&pidPitchRate, 0);
 
+  studentPidInit(&pidYawRate, RATE_YAW_KP, RATE_YAW_KI, RATE_YAW_KD, updateDt);
+  studentPidSetIntegralLimit(&pidYawRate, 0);
 
-    // 488 TODO Constrain the integral (unless the integral limit is zero), use the constrain function
-    
+  studentPidInit(&pidRoll, ATT_ROLL_KP, ATT_ROLL_KI, ATT_ROLL_KD, updateDt);
+  studentPidSetIntegralLimit(&pidRoll, 0);
 
-    // 488 TODO Constrain the total PID output (unless the output Limit is zero)
+  studentPidInit(&pidPitch, ATT_PITCH_KP, ATT_PITCH_KI, ATT_PITCH_KD, updateDt);
+  studentPidSetIntegralLimit(&pidPitch, 0);
 
+  studentPidInit(&pidYaw, ATT_YAW_KP, ATT_YAW_KI, ATT_YAW_KD, updateDt);
+  studentPidSetIntegralLimit(&pidYaw, 0);
 
-    return 0.0;
+  isInit = true;
 }
 
-/**
- * Set the integral limit for this PID in deg.
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] limit Pid integral swing limit.
- */
-void studentPidSetIntegralLimit(PidObject* pid, const float limit) {
-  // 488 TODO
-  if(pid.ki + pid.error < limit){
-    pid.ki = pid.ki + pid.error;
-  }
-
-}
-
-/**
- * Reset the PID error values
- *
- * @param[in] pid   A pointer to the pid object.
- */
-void studentPidReset(PidObject* pid)
+bool studentAttitudeControllerTest()
 {
-  // 488 TODO
+  return isInit;
 }
 
-/**
- * Set a new error. Use if a special error calculation is needed.
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] error The new error
- */
-void studentPidSetError(PidObject* pid, const float error)
+static float capAngle(float angle)
 {
-  pid.error = error;
+  while (angle > 180.0f) angle -= 360.0f;
+  while (angle < -180.0f) angle += 360.0f;
+  return angle;
 }
 
-/**
- * Set a new set point for the PID to track.
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] angle The new set point
- */
-void studentPidSetDesired(PidObject* pid, const float desired)
+void studentAttitudeControllerCorrectAttitudePID(
+       float eulerRollActual, float eulerPitchActual, float eulerYawActual,
+       float eulerRollDesired, float eulerPitchDesired, float eulerYawDesired,
+       float* rollRateDesired, float* pitchRateDesired, float* yawRateDesired)
 {
-  // 488 TODO
-  pid->setpoint = desired;
+  *rollRateDesired = studentPidUpdate(&pidRoll, eulerRollDesired - eulerRollActual);
+  *pitchRateDesired = studentPidUpdate(&pidPitch, eulerPitchDesired - eulerPitchActual);
+
+  float yawError = capAngle(eulerYawDesired - eulerYawActual);
+  *yawRateDesired = studentPidUpdate(&pidYaw, yawError);
 }
 
-/**
- * Get the current desired setpoint
- * 
- * @param[in] pid  A pointer to the pid object.
- * @return The set point
- */
-float studentPidGetDesired(PidObject* pid)
+void studentAttitudeControllerCorrectRatePID(
+       float rollRateActual, float pitchRateActual, float yawRateActual,
+       float rollRateDesired, float pitchRateDesired, float yawRateDesired,
+       int16_t* rollCmd, int16_t* pitchCmd, int16_t* yawCmd)
 {
-  // 488 TODO
-  return pid->desired;
+  float rollCmdRaw = studentPidUpdate(&pidRollRate, rollRateDesired - rollRateActual);
+  float pitchCmdRaw = studentPidUpdate(&pidPitchRate, pitchRateDesired - pitchRateActual);
+  float yawCmdRaw = studentPidUpdate(&pidYawRate, yawRateDesired - yawRateActual);
+
+  *rollCmd = saturateSignedInt16(rollCmdRaw);
+  *pitchCmd = saturateSignedInt16(pitchCmdRaw);
+  *yawCmd = saturateSignedInt16(yawCmdRaw);
 }
 
-
-/**
- * Find out if PID is active
- * @return TRUE if active, FALSE otherwise
- */
-bool studentPidIsActive(PidObject* pid)
+void studentAttitudeControllerResetRollAttitudePID(void)
 {
-  //488 TODO is active if the constants kp ki kd are above some small threshold
-  float threshold = .5
-  if(abs(pid.ki) > threshold && abs(pid.kd) > threshold && abs(pid.kp) > threshold){
-    return true;
-  }
-  else{
-    return false;
-  }
+  studentPidReset(&pidRoll);
 }
 
-/**
- * Set a new proportional gain for the PID.
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] kp    The new proportional gain
- */
-void studentPidSetKp(PidObject* pid, const float kp)
+void studentAttitudeControllerResetYawAttitudePID(void)
 {
-  // 488 TODO
-  pid.kp = kp;
+  studentPidReset(&pidYaw);
 }
 
-/**
- * Set a new integral gain for the PID.
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] ki    The new integral gain
- */
-void studentPidSetKi(PidObject* pid, const float ki)
+void studentAttitudeControllerResetPitchAttitudePID(void)
 {
-  // 488 TODO
-  pid.ki = ki;
+  studentPidReset(&pidPitch);
 }
 
-/**
- * Set a new derivative gain for the PID.
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] kd    The derivative gain
- */
-void studentPidSetKd(PidObject* pid, const float kd)
+void studentAttitudeControllerResetAllPID(void)
 {
-  // 488 TODO
-  pid.kd = kd;
+  studentPidReset(&pidRoll);
+  studentPidReset(&pidPitch);
+  studentPidReset(&pidYaw);
+  studentPidReset(&pidRollRate);
+  studentPidReset(&pidPitchRate);
+  studentPidReset(&pidYawRate);
 }
 
 /**
- * Set a new dt gain for the PID. Defaults to IMU_UPDATE_DT upon construction
- *
- * @param[in] pid   A pointer to the pid object.
- * @param[in] dt    Delta time
+ *  Log variables of attitude PID controller
+ */ 
+LOG_GROUP_START(s_pid_attitude)
+/**
+ * @brief Proportional output roll
  */
-void studentPidSetDt(PidObject* pid, const float dt) {
-  // 488 TODO
-  pid.
-  IMU_UPDATE_DT;
-}
+LOG_ADD(LOG_FLOAT, roll_outP, &pidRoll.outP)
+/**
+ * @brief Integral output roll
+ */
+LOG_ADD(LOG_FLOAT, roll_outI, &pidRoll.outI)
+/**
+ * @brief Derivative output roll
+ */
+LOG_ADD(LOG_FLOAT, roll_outD, &pidRoll.outD)
+/**
+ * @brief Proportional output pitch
+ */
+LOG_ADD(LOG_FLOAT, pitch_outP, &pidPitch.outP)
+/**
+ * @brief Integral output pitch
+ */
+LOG_ADD(LOG_FLOAT, pitch_outI, &pidPitch.outI)
+/**
+ * @brief Derivative output pitch
+ */
+LOG_ADD(LOG_FLOAT, pitch_outD, &pidPitch.outD)
+/**
+ * @brief Proportional output yaw
+ */
+LOG_ADD(LOG_FLOAT, yaw_outP, &pidYaw.outP)
+/**
+ * @brief Intergal output yaw
+ */
+LOG_ADD(LOG_FLOAT, yaw_outI, &pidYaw.outI)
+/**
+ * @brief Derivative output yaw
+ */
+LOG_ADD(LOG_FLOAT, yaw_outD, &pidYaw.outD)
+LOG_GROUP_STOP(s_pid_attitude)
+
+/**
+ *  Log variables of attitude rate PID controller
+ */
+LOG_GROUP_START(s_pid_rate)
+/**
+ * @brief Proportional output roll rate
+ */
+LOG_ADD(LOG_FLOAT, roll_outP, &pidRollRate.outP)
+/**
+ * @brief Integral output roll rate
+ */
+LOG_ADD(LOG_FLOAT, roll_outI, &pidRollRate.outI)
+/**
+ * @brief Derivative output roll rate
+ */
+LOG_ADD(LOG_FLOAT, roll_outD, &pidRollRate.outD)
+/**
+ * @brief Proportional output pitch rate
+ */
+LOG_ADD(LOG_FLOAT, pitch_outP, &pidPitchRate.outP)
+/**
+ * @brief Integral output pitch rate
+ */
+LOG_ADD(LOG_FLOAT, pitch_outI, &pidPitchRate.outI)
+/**
+ * @brief Derivative output pitch rate
+ */
+LOG_ADD(LOG_FLOAT, pitch_outD, &pidPitchRate.outD)
+/**
+ * @brief Proportional output yaw rate
+ */
+LOG_ADD(LOG_FLOAT, yaw_outP, &pidYawRate.outP)
+/**
+ * @brief Integral output yaw rate
+ */
+LOG_ADD(LOG_FLOAT, yaw_outI, &pidYawRate.outI)
+/**
+ * @brief Derivative output yaw rate
+ */
+LOG_ADD(LOG_FLOAT, yaw_outD, &pidYawRate.outD)
+LOG_GROUP_STOP(s_pid_rate)
+
+/**
+ * Tuning settings for the gains of the PID
+ * controller for the attitude of the Crazyflie which consists
+ * of the Yaw Pitch and Roll 
+ */
+PARAM_GROUP_START(s_pid_attitude)
+/**
+ * @brief Proportional gain for the PID roll controller
+ */
+PARAM_ADD(PARAM_FLOAT, roll_kp, &pidRoll.kp)
+/**
+ * @brief Integral gain for the PID roll controller
+ */
+PARAM_ADD(PARAM_FLOAT, roll_ki, &pidRoll.ki)
+/**
+ * @brief Derivative gain for the PID roll controller
+ */
+PARAM_ADD(PARAM_FLOAT, roll_kd, &pidRoll.kd)
+/**
+ * @brief Proportional gain for the PID pitch controller
+ */
+PARAM_ADD(PARAM_FLOAT, pitch_kp, &pidPitch.kp)
+/**
+ * @brief Integral gain for the PID pitch controller
+ */
+PARAM_ADD(PARAM_FLOAT, pitch_ki, &pidPitch.ki)
+/**
+ * @brief Derivative gain for the PID pitch controller
+ */
+PARAM_ADD(PARAM_FLOAT, pitch_kd, &pidPitch.kd)
+/**
+ * @brief Proportional gain for the PID yaw controller
+ */
+PARAM_ADD(PARAM_FLOAT, yaw_kp, &pidYaw.kp)
+/**
+ * @brief Integral gain for the PID yaw controller
+ */
+PARAM_ADD(PARAM_FLOAT, yaw_ki, &pidYaw.ki)
+/**
+ * @brief Derivative gain for the PID yaw controller
+ */
+PARAM_ADD(PARAM_FLOAT, yaw_kd, &pidYaw.kd)
+PARAM_GROUP_STOP(s_pid_attitude)
+
+/**
+ * Tuning settings for the gains of the PID controller for the rate angles of
+ * the Crazyflie, which consists of the yaw, pitch and roll rates 
+ */
+PARAM_GROUP_START(s_pid_rate)
+/**
+ * @brief Proportional gain for the PID roll rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, roll_kp, &pidRollRate.kp)
+/**
+ * @brief Integral gain for the PID roll rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, roll_ki, &pidRollRate.ki)
+/**
+ * @brief Derivative gain for the PID roll rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, roll_kd, &pidRollRate.kd)
+/**
+ * @brief Proportional gain for the PID pitch rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, pitch_kp, &pidPitchRate.kp)
+/**
+ * @brief Integral gain for the PID pitch rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, pitch_ki, &pidPitchRate.ki)
+/**
+ * @brief Derivative gain for the PID pitch rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, pitch_kd, &pidPitchRate.kd)
+/**
+ * @brief Proportional gain for the PID yaw rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, yaw_kp, &pidYawRate.kp)
+/**
+ * @brief Integral gain for the PID yaw rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, yaw_ki, &pidYawRate.ki)
+/**
+ * @brief Derivative gain for the PID yaw rate controller
+ */
+PARAM_ADD(PARAM_FLOAT, yaw_kd, &pidYawRate.kd)
+PARAM_GROUP_STOP(s_pid_rate)
