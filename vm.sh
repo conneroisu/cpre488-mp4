0e2ea27afe5893ce11e3e181d90fc7890f28bb39
conneroisu <88785126+conneroisu@users.noreply.github.com>
2025-04-21 07:48:09 -0500

    Update vm.sh
    added script from branch compilerme for coping files automatically to the correct spots for part 2 firmware

--- vm.sh
+++ vm.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-#
-# 
+# Running from the root of the cloned repo which should be reflected onto the vm in some folder run `sh vm.sh`.
 
-curl https://class.ece.iastate.edu/cpre488/labs/488_MP-4_VM_S24_Final.txt
+
+cp ./src/controller_student.c ~/MicroCART/crazyflie_software/crazyflie-firmware-lab-part-2/src/modules/src/controller_student.c
+cp ./src/student_attitude_controller.c ~/MicroCART/crazyflie_software/crazyflie-firmware-lab-part-2/src/modules/src/student_attitude_controller.c
+cp ./src/student_pid.c ../MicroCART/crazyflie_software/crazyflie-firmware-lab-part-2/src/modules/src/student_pid.c
+
+cp ./src/student_attitude_controller.h ~/MicroCART/crazyflie_software/crazyflie-firmware-lab-part-2/src/modules/interface/student_attitude_controller.h
+cp ./src/student_pid.h ~/MicroCART/crazyflie_software/crazyflie-firmware-lab-part-2/src/modules/interface/student_pid.h
 
